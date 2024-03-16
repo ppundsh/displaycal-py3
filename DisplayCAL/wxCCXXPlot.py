f127ef56b285ca87a969f3ed0770236cbc2354ab
ethanbrookins <ethanbrookins@users.noreply.github.com>
2024-03-15 17:24:11 -0700

    Adds checks so the matrix graph will still draw even if the .ccmx does not contain certain values

--- DisplayCAL/wxCCXXPlot.py
+++ DisplayCAL/wxCCXXPlot.py
@@ -346,13 +346,13 @@ class CCXXPlot(wx.Frame):
             x_label = [lang.getstr("matrix")]
             x_label.extend(["%9.6f %9.6f %9.6f" % tuple(row) for row in mtx])
             if ref:
-                ref_observer = cgats.queryv1("REFERENCE_OBSERVER").decode("utf-8")
-                if ref_observer:
-                    ref += ", " + observers_ab.get(ref_observer, ref_observer)
+                ref_observer = cgats.queryv1("REFERENCE_OBSERVER")
+                if ref_observer is not None: # If the .ccmx does not contain this value, the graph will still draw
+                    ref += ", " + observers_ab.get(ref_observer.decode("utf-8"), ref_observer.decode("utf-8"))
                 x_label.append("")
                 x_label.append(ref)
-            fit_method = cgats.queryv1("FIT_METHOD").decode("utf-8")
-            if fit_method == "xy":
+            fit_method = cgats.queryv1("FIT_METHOD")
+            if fit_method is not None and fit_method.decode("utf-8") == "xy": # If the .ccmx does not contain this value, the graph will still draw
                 fit_method = lang.getstr("ccmx.use_four_color_matrix_method")
             elif fit_method:
                 fit_method = lang.getstr("perceptual")
